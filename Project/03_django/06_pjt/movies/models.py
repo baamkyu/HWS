731f6ccd174ebf2569b572b44ecc1b861b9fe075
baamkyu <baamkyu@gmail.com>
2022-10-14 21:33:23 +0900

    221014

---
+++ Project/03_django/06_pjt/movies/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+  title=models.CharField(max_length=20)
+  audience=models.IntegerField()
+  release_data=models.DateField()
+  genre=models.CharField(max_length=30)
+  score=models.FloatField()
+  poster_url=models.TextField()
+  description=models.TextField()
