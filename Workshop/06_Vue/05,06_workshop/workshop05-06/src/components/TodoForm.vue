32c85360f657970ac106c0b48fad352a9e6c909f
baamkyu <baamkyu@gmail.com>
2022-11-10 17:46:00 +0900

    hws

---
+++ Workshop/06_Vue/05,06_workshop/workshop05-06/src/components/TodoForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text"
+    v-model.trim="todoTitle"
+    @keyup.enter="createTodo"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  methods: {
+    createTodo() {
+      this.$store.dispatch('createTodo', this.todoTitle)
+      this.todoTitle = null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
